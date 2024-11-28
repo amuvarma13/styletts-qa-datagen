33f3657fa3ce60521624edcdc2b3f4e111d2f5bc
Amartya Varma <amartya.varma@gmail.com>
2024-11-28 15:38:12 -0800

    f

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from scipy.io.wavfile import write
 import msinference 
 import random
+import nltk
+nltk.download('punkt_tab')
 text = 'Hello world!'
 voices_strings = ["f-us-1.wav", "f-us-2.wav", "f-us-3.wav", "f-us-4.wav", "m-us-1.wav", "m-us-2.wav", "m-us-3.wav", "m-us-4.wav"]
 voices = [msinference.compute_style("voices/"+voice) for voice in voices_strings]
