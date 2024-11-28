da7d1925129b9d575a82197520855e8dede4f0f4
Amartya Varma <amartya.varma@gmail.com>
2024-11-28 15:35:04 -0800

    f

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from scipy.io.wavfile import write
 import msinference 
+import random
 text = 'Hello world!'
-voice = msinference.compute_style('voice.wav')
+voices_strings = ["f-us-1.wav", "f-us-2.wav", "f-us-3.wav", "f-us-4.wav", "m-us-1.wav", "m-us-2.wav", "m-us-3.wav", "m-us-4.wav"]
+voices = [msinference.compute_style(voice) for voice in voices_strings]
+voice = random.choice(voices)
 wav = msinference.inference(text, voice, alpha=0.3, beta=0.7, diffusion_steps=7, embedding_scale=1)
 write('result.wav', 24000, wav)
