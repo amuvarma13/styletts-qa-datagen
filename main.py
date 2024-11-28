cb7245909e75c704a2c63fe1c70c0635731b5a46
Amartya Varma <amartya.varma@gmail.com>
2024-11-28 15:30:53 -0800

    f

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from scipy.io.wavfile import write
-import StyleTTS2.msinference as msinference
+import msinference 
 text = 'Hello world!'
 voice = msinference.compute_style('voice.wav')
 wav = msinference.inference(text, voice, alpha=0.3, beta=0.7, diffusion_steps=7, embedding_scale=1)
