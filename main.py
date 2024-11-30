0badc0599c4450d851625eebcb1098716c77adff
Amartya Varma <amartya.varma@gmail.com>
2024-11-30 12:06:51 -0800

    f

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ text = 'Hello world!'
 voices_strings = ["f-us-1.wav", "f-us-2.wav", "f-us-3.wav", "f-us-4.wav", "m-us-1.wav", "m-us-2.wav", "m-us-3.wav", "m-us-4.wav"]
 voices = [msinference.compute_style("voices/"+voice) for voice in voices_strings]
 dsn = "amuvarma/qa_large_0_5_speechq"
-push_name = "amuvarma/qa_large_0_4_speechqa-both"
+push_name = "amuvarma/qa_large_0_5_speechqa-both"
 
 ds = load_dataset(dsn, split='train')
 ds = ds.shuffle(seed=42)
